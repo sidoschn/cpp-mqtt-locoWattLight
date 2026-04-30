4176a9a2fb6d576e2c3e088050dee10705712723
sidoschn <dominik.schneidereit@fau.de>
2026-04-30 09:10:56 +0200

    actually added "WEBSERVERPORT" in svr.listen command and made WEBSERVERPORT int from const int

--- main2.cpp
+++ main2.cpp
@@ -10,7 +10,7 @@
 #include "json.hpp"
 
 
-const int WEBSERVERPORT(8234);
+int WEBSERVERPORT(8234);
 const std::string DFLT_SERVER_URI("mqtt://192.168.39:1883");
 const std::string CLIENT_ID("cpp_publisher");
 const std::string TOPIC("cppTest/testTopic");
@@ -349,7 +349,8 @@ int main()
     }
     
 
-    svr.listen("0.0.0.0", );
+    svr.listen("0.0.0.0", WEBSERVERPORT);
+    
     
     // mqtt::async_client client(SERVER_ADDRESS, CLIENT_ID);
 
