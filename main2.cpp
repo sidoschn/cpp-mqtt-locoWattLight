28dae8e5b031efea5b90663e727d89e6b3dab1f6
sidoschn <dominik.schneidereit@fau.de>
2026-04-30 09:03:27 +0200

    changed the webserver listen port to 80, moved "/hi" to default page "/"

--- main2.cpp
+++ main2.cpp
@@ -10,7 +10,7 @@
 #include "json.hpp"
 
 
-const std::string SERVER_ADDRESS("tcp://192.168.0.39:1883");
+const int WEBSERVERPORT(80);
 const std::string DFLT_SERVER_URI("mqtt://192.168.39:1883");
 const std::string CLIENT_ID("cpp_publisher");
 const std::string TOPIC("cppTest/testTopic");
@@ -318,7 +318,7 @@ int main()
     // fileStream.close();
     // htmlString = htmlDoc;
     
-    svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
+    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
         res.set_content(SMAINPAGE, "text/html");
         });
     httplib::Server::Handler serverHandler;
@@ -349,7 +349,7 @@ int main()
     }
     
 
-    svr.listen("0.0.0.0", 8234);
+    svr.listen("0.0.0.0", WEBSERVERPORT);
     
     // mqtt::async_client client(SERVER_ADDRESS, CLIENT_ID);
 
